7824a6f2d5800f9e79e873273d4ce7b369064e49
sanayut12 <sanayut12@gmail.com>
2021-02-07 16:07:29 +0700

    update imagebase64

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var mysql = require('mysql')
 var app = express()
-
+var fs = require('fs');
 
 //start connect database
 
@@ -38,12 +38,12 @@ app.post('/login',(req,res)=>{
     console.log("login event")
 
     var data = {
-        username: req.body.username,
+        email: req.body.email,
         password: req.body.password 
     }
 
-    var sql_code = "SELECT Shop_ID , User_name ,Password FROM shop_info WHERE User_name = '"+data.username+"'AND Password='"+data.password+"';"
-
+    var sql_code = "SELECT Shop_ID , Email ,Password FROM shop_info WHERE Email = '"+data.email+"'AND Password='"+data.password+"';"
+  
     connect_db.query(sql_code,function(err,result){
         if(err) console.log(err);        
         console.log(result)
@@ -67,28 +67,35 @@ app.post('/login',(req,res)=>{
 
 //register       (username , password,ชื่อร้าน,เบอร์โทร , email , ที่อยู่)
 
-app.post('/register',(req,res)=>{
+app.post('/register',async (req,res)=>{
     console.log('register event')
 
     var data = {
-        username : req.body.username,
         password : req.body.password,
         shop_name : req.body.shop_name,
         phone : req.body.phone,
         email : req.body.email,
+        image : await req.body.image,
         address : req.body.address
     }
-    var sql_code = "INSERT INTO shop_info (User_name,Password,Shop_name,Phone,Email,Address) VALUES ('"+data.username+"',"+data.password+",'"+data.shop_name+"',"+data.phone+",'"+data.email+"','"+data.address+"')"
+    // fs.writeFile('/image/')
+    var imagedir = fs.readdirSync("./image").length
+    fs.writeFileSync("./image/"+imagedir+".jpg",data.image,'base64',function(err){
+        if (err) console.log(err)
+    })
+    var sql_code = "INSERT INTO shop_info (Password,Shop_name,Phone,Email,Address,Url) VALUES ("+data.password+",'"+data.shop_name+"',"+data.phone+",'"+data.email+"','"+data.address+"','image/"+imagedir+".jpg')"
     connect_db.query(sql_code,function(err,result){
         if (err){
             console.log(err);
             var response = {
+                data : imagedir,
                 status:false
             }
             res.send(response)
         }else{
             //console.log(result)  //show data from client 
             var response = {
+                data : imagedir,
                 status:true
             }
             res.send(response)
@@ -101,11 +108,19 @@ app.post('/register',(req,res)=>{
 
 app.post('/get_profile',(req,res)=>{
     var data = {
-        shop_ID : req.body.shop_ID
+        shop_id : req.body.shop_id
     }
 
-    var sql_code = "SELECT User_name ,Shop_name,Phone , Email,Address FROM shop_info WHERE Shop_ID = "+data.shop_name
+    var sql_code = "SELECT Shop_name,Phone, Url , Email,Address FROM shop_info WHERE Shop_ID = "+data.shop_id
     connect_db.query(sql_code,function(err,result){
+
+        ///read file and convert image to base64 ;return base64 type string
+        var imagetobase64 =  fs.readFileSync(result[0]['Url'],{encoding : 'base64'})
+
+        //del path url from database  and set image into object
+        delete result[0]['Url']
+        result[0]["image"] = imagetobase64
+
         if (err){
             console.log(err);
             var response = {
@@ -126,11 +141,20 @@ app.post('/get_profile',(req,res)=>{
 
 app.post('/add_product',(req,res)=>{
     var data = {
-        shop_ID:req.body.shop_ID,
-        Product_name : req.body.Product_name,
-        url : req.body.url
+        shop_id:req.body.shop_id,
+        product_name : req.body.product_name,
+        image : req.body.image,
+        description : req.body.description
     }
-    var sql_code = "INSERT INTO products (shop_ID,Product_name,url) VALUES ("+data.shop_ID+",'"+data.Product_name+"','"+data.url+"')"
+
+    // fs.writeFile('/image/')
+    var imagedir = fs.readdirSync("./image").length
+    fs.writeFileSync("./image/"+imagedir+".jpg",data.image,'base64',function(err){
+        if (err) console.log(err)
+    })
+
+
+    var sql_code = "INSERT INTO products (shop_ID,Product_name,Url,Description) VALUES ("+data.shop_id+",'"+data.product_name+"','image/"+imagedir+".jpg','"+data.description+"')"
     connect_db.query(sql_code,function(err,result){
         if (err){
             console.log(err);
@@ -149,9 +173,13 @@ app.post('/add_product',(req,res)=>{
 
 app.post('/get_product',(req,res)=>{
     var data = {
-        shop_ID : req.body.shop_ID
+        shop_id : req.body.shop_id
     }
-    var sql_code = "SELECT Product_ID, Product_name , url FROM products WHERE Shop_ID = "+data.shop_ID
+    var sql_code = "SELECT Product_ID, Product_name , Url ,	Description FROM products WHERE Shop_ID = "+data.shop_id
+
+    
+
+
     connect_db.query(sql_code,function(err,result){
         if (err){
             console.log(err);
@@ -161,6 +189,15 @@ app.post('/get_product',(req,res)=>{
             }
             res.send(response)
         }else{
+
+            
+            for(var i = 0; i < result.length;i++){
+                var imagetobase64 = fs.readFileSync(result[i]['Url'],{encoding : 'base64'})
+                delete result[i]['Url']
+                result[i]["image"] = imagetobase64
+            }
+                       
+
             var response = {
                 data :result,
                 status:true
@@ -172,6 +209,22 @@ app.post('/get_product',(req,res)=>{
 
 //feed
 
+app.get('/image',async (req,res)=>{
+    var data = {
+        shop_id : await req.body.shop_id
+    }
+
+    var sql_code = "SELECT Url from shop_info WHERE Shop_ID = "+data.shop_id
+
+    connect_db.query(sql_code,function(err,result){
+        if(err) console.log(err)
+        console.log(result[0]['Url'])
+        var imagetobase64 =  fs.readFileSync(result[0]['Url'],{encoding : 'base64'})
+        res.send({
+            image : imagetobase64
+        })
+    })
+})
 
 
 
